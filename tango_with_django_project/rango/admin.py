911e8b71b60997d86f76473e3e3bc21ef3718333
AsterB <bentonaster@gmail.com>
2017-05-28 15:13:10 +0200

    third

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name', 'views', 'likes']
+    list_display = ('name', 'views', 'likes')
+
+admin.site.register(Category, CategoryAdmin)
+
+class PageAdmin(admin.ModelAdmin):
+    fields = ['category', 'title', 'url', 'views']
+    list_display = ('title', 'category', 'url', 'views')
+
+admin.site.register(Page, PageAdmin)
